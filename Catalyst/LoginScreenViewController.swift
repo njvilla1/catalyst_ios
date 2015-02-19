3c427db3e04182852adf27d07a80349d461555be
Tyler Hessler <thessler@host-155-246-138-66.dhcp.stevens-tech.edu>
2015-02-19 00:08:04 -0500

    added login screen with segue

---
+++ Catalyst/LoginScreenViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginScreenViewController.swift
+//  Catalyst
+//
+//  Created by Tyler Hessler on 2/18/15.
+//  Copyright (c) 2015 Design9. All rights reserved.
+//
+
+import UIKit
+
+class LoginScreenViewController: NSObject {
+   
+}

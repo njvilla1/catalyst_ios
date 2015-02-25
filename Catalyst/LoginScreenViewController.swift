6390d63a38aa1be770f791a3cc364a9ebc9071c1
Nick Villa <njvilla1@gmail.com>
2015-02-24 22:40:19 -0500

    working on network authentication, getting 500 error

--- Catalyst/LoginScreenViewController.swift
+++ Catalyst/LoginScreenViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 
-class LoginScreenViewController: UIViewController {
+class LoginScreenViewController: UIViewController, APIControllerProtocol {
     @IBOutlet var loginButton: UIButton!
     @IBOutlet var usernameField: UITextField!
+    @IBOutlet var passwordField: UITextField!
+    
+    var username = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,17 +23,39 @@ class LoginScreenViewController: UIViewController {
         
     }
     
+    //get and use results from api call
+    func didReceiveAPIResults(results: NSDictionary) {
+        println("Received API Results")
+        
+        if (results["authentication"] as NSString == "success") {
+            println("we made it to the delegate callback function, result is success")
+            println("go to profile feed, authentication successful")
+            //segue goes here
+            performSegueWithIdentifier("loginSegue", sender: nil)
+        }
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     @IBAction func loginButtonClicked(sender: AnyObject) {
+        println("login button clicked")
+        username = usernameField.text
+        println("Username is: \(username)")
+        
+        let password = passwordField.text
+        println("Password is:\(password)")
+        
+        let apicontroller = APIController()
+        apicontroller.post(["username":username, "password":password, "userLatitude":"0.0", "userLongitude":"0.0"], url: "http://ec2-54-172-188-199.compute-1.amazonaws.com/catalyst/login/")
         
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
-        let username = usernameField.text
+        
         let destinationVC = segue.destinationViewController as ProfileFeedViewController
         destinationVC.username = username
         
